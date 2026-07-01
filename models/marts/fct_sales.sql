7cca1384b8e452f8a953ee55fc73981e20c0567b
minhnnguyen003 <minhnnguyen003@gmail.com>
2026-07-01 10:26:15 +0700

    modified:   models/marts/fct_sales.sql

--- models/marts/fct_sales.sql
+++ models/marts/fct_sales.sql
@@ -1,5 +1,21 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='merge',
+        unique_key='order_id'
+    )
+}}
+
 select
     order_date,
     sum(total_amount) as revenue
 from {{ ref('stg_orders') }}
+
+{% if is_incremental() %}
+where order_date >= (
+    select max(order_date)
+    from {{ this }}
+)
+{% endif %}
+
 group by order_date
